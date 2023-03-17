28e9920aa203cbb4646918caab1428dc35838b3f
Chamy619 <yd95069506@naver.com>
2023-03-17 19:24:22 +0900

    2-3 함수

---
+++ src/Variable.kt
@@ -0,0 +1,28 @@
+package src
+
+fun sum(a: Int, b: Int) : Int {
+    return a + b;
+}
+
+fun sum2(a: Int, b: Int) = a + b
+
+fun greeting(message: String = "안녕하세요") {
+    println(message)
+}
+
+fun log(level: String = "INFO", message: String) {
+    println("[$level] $message")
+}
+
+fun main() {
+    println(sum(1, 2))
+    println(sum2(5, 6))
+
+    greeting()
+    greeting("안녕")
+
+    log(message = "인포 로그")
+    log(level = "DEBUG", message = "디버그 로그")
+    log("WARN", "경고 로그")
+    log(level = "ERROR", "에러 로그")
+}
