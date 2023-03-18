9751a5fbcb108d34343caa380cb42ce86973a371
Chamy619 <yd95069506@naver.com>
2023-03-18 14:37:46 +0900

    2-6 예외처리

---
+++ src/Exception.kt
@@ -0,0 +1,32 @@
+package src
+
+fun main() {
+
+    try {
+        throw Exception()
+    } catch (e: Exception) {
+        println("에러 발생!")
+    } finally {
+        println("Finally 실행!")
+    }
+
+    val a = try {
+        "1234".toInt()
+    } catch (e: Exception) {
+        println("예외 발생!")
+    }
+
+    println(a)
+
+//    throw Exception("예외 발생")
+//    failFast("예외 발생!")
+
+    val b: String? = null
+    val c: String = b ?: failFast("b is null")
+
+    println(c.length)
+}
+
+fun failFast(str: String): Nothing {
+    throw IllegalArgumentException(str)
+}
