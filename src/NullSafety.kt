8f468962287e5d555ada22acfd62fd5a71e0b6a4
Chamy619 <yd95069506@naver.com>
2023-03-18 14:21:20 +0900

    2-5 널 안전성

---
+++ src/NullSafety.kt
@@ -0,0 +1,32 @@
+package src
+
+fun getNullString(): String? = null
+
+fun getLengthIfNotNull(str: String?): Int = str?.length ?: 0
+
+fun main() {
+//    val a: String = null
+//    var b: String = "abcd"
+//    b = null
+
+    var a: String? = null
+    val length = a?.length
+    println(length)
+
+    val b: Int = if (a != null) a.length else 0
+    println(b)
+
+    val c: Int = a?.length ?: 0
+    println(c)
+
+    val nullableStr = getNullString()
+
+    val nullableStrLength = getLengthIfNotNull(nullableStr)
+
+    println(nullableStrLength)
+
+//    throw NullPointerException()
+
+//    val d: String? = null;
+//    val e = d!!.length
+}
