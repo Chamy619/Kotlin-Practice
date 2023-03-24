34b450b5c7be8bf1be7098742c41787ae58c2fb5
Chamy619 <yd95069506@naver.com>
2023-03-24 22:01:58 +0900

    2-2-3 싱글톤과 동반객체

---
+++ src/Singleton.kt
@@ -0,0 +1,54 @@
+package src
+
+import java.time.LocalDateTime
+
+object Singleton {
+
+    val a = 1234
+
+    fun printA() = println(a)
+}
+
+object DatetimeUtils {
+
+    val now: LocalDateTime
+        get() = LocalDateTime.now()
+
+    const val DEFAULT_FORMAT = "YYYY-MM-DD"
+
+    fun same(a: LocalDateTime, b: LocalDateTime): Boolean {
+        return a == b
+    }
+}
+
+class MyClass {
+
+    private constructor()
+
+    companion object MyCompanion {
+        val a = 1234
+
+        fun newInstance() = MyClass()
+    }
+}
+
+fun main() {
+    println(Singleton.a)
+    Singleton.printA()
+
+    println(DatetimeUtils.now)
+    println(DatetimeUtils.now)
+    println(DatetimeUtils.now)
+
+    println(DatetimeUtils.DEFAULT_FORMAT)
+
+    val now = LocalDateTime.now()
+    println(DatetimeUtils.same(now, now))
+    println(DatetimeUtils.same(now, DatetimeUtils.now))
+
+    println(MyClass.a)
+    println(MyClass.newInstance())
+
+    println(MyClass.MyCompanion.a)
+    println(MyClass.MyCompanion.newInstance())
+}
