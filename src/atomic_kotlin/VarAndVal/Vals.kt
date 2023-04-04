64147cb6c54d1ab7b9b47962ec04a29865ed4f3f
Chamy619 <yd95069506@naver.com>
2023-04-04 20:18:16 +0900

    Atom 4 var와 val

---
+++ src/atomic_kotlin/VarAndVal/Vals.kt
@@ -0,0 +1,10 @@
+package src.atomic_kotlin.VarAndVal
+
+fun main() {
+    val whole = 11
+    val fractional = 1.4
+    val words = "Twas Brilling"
+    println(whole)
+    println(fractional)
+    println(words)
+}
---
+++ src/atomic_kotlin/VarAndVal/Vars.kt
@@ -0,0 +1,10 @@
+package src.atomic_kotlin.VarAndVal
+
+fun main() {
+    var whole = 11
+    var fractional = 1.4
+    var words = "Twas Brilling"
+    println(whole)
+    println(fractional)
+    println(words)
+}
