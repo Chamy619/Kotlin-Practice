b1f795a1d6587ace95d55584828263239307936e
Chamy619 <yd95069506@naver.com>
2023-04-04 20:22:59 +0900

    Atom 5 데이터 타입

---
+++ src/atomic_kotlin/DataTypes/StringPlusNumber.kt
@@ -0,0 +1,5 @@
+package src.atomic_kotlin.DataTypes
+
+fun main() {
+    println("Sally" + 5.9)
+}
---
+++ src/atomic_kotlin/DataTypes/Types.kt
@@ -0,0 +1,20 @@
+package src.atomic_kotlin.DataTypes
+
+fun main() {
+    val whole: Int = 11
+    val fractional: Double = 1.4
+    val trueOrFalse: Boolean = true
+    val words: String = "A value"
+    val character: Char = 'z'
+    val lines: String = """Triple Quotes let
+you have many lines
+in your string
+    """
+
+    println(whole)
+    println(fractional)
+    println(trueOrFalse)
+    println(words)
+    println(character)
+    println(lines)
+}
